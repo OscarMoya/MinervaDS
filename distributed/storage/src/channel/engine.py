bbe59ae1d39a96b5745bfc9a5fca02f27d13c7b9
Oscar Moya <oscar.moya@i2cat.net>
2015-01-15 16:54:02 +0100

    adding Channel Type engine and simple Client Manager

---
+++ distributed/storage/src/channel/engine.py
@@ -0,0 +1,24 @@
+import os
+import re
+import types
+
+class ChannelEngine:
+
+    def __init__(self):
+
+    def load_type(self, channel_type):
+        channel_module = None
+        model_dir = os.path.dirname( __file__) + "../types/" #ChannelTypes Location Location
+        for filename in os.listdir(model_dir):
+            if filename == self.__get_python_file(channel_type):
+                # Import the model file and find all clases inside it.
+                channel_module = __import__(channel_type)
+                break;
+        if channel_module:
+            return channel_module.launch()
+        else:
+            raise Exception("Channel Type Not Found")
+
+
+
+
