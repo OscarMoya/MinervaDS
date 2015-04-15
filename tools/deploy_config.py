2a0ba4fc25215ca30b9f9c6a5f0c26a96d52d25b
Carolina Fernandez <carolina.fernandez@i2cat.net>
2015-04-15 19:24:18 +0200

    Uploading proper deploy_config.py file

---
+++ tools/deploy_config.py
@@ -0,0 +1,26 @@
+## Fill this file with the IP of your three
+# storage nodes (for both A, B, AxB chunks) 
+# and the controller.
+#
+# Add also the password and SSH port used
+# to access the machines. This information 
+# should be the same for every machine.
+
+
+# Place IPs an password as a string:
+# Example:
+#
+# SERVER_A_IP = "192.168.1.123"
+# PASSWORD ="fancypassword"
+
+SERVER_A_IP = 
+SERVER_B_IP = 
+SERVER_C_IP = 
+CONTROLLER_IP = 
+
+SSH_PASSWORD = 
+
+# Set a number for the SSH port
+# (default: 22)
+
+SSH_PORT = 
